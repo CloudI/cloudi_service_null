c50586ba5b73b1caf86cf7870c1c0390f866ba05
Michael Truog <mjtruog@gmail.com>
2017-09-12 19:31:45 -0700

    Version 1.7.2.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceNull do
 
   def project do
     [app: :cloudi_service_null,
-     version: "1.7.1",
+     version: "1.7.2",
      language: :erlang,
      description: description(),
      package: package(),
@@ -14,7 +14,7 @@ defmodule CloudIServiceNull do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.7.1"}]
+    [{:cloudi_core, "~> 1.7.2"}]
   end
 
   defp description do
